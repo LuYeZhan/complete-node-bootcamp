49bcde52f636a2af0b7984d50bda57035f860d6d
Ye Zhan <lu@hooli.store>
2019-12-09 11:33:30 +0100

    routing concepts

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 
 
@@ -34,7 +35,20 @@ const http = require('http');
 ///////////////////////
 // Server
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+    
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+     } else if (pathName === '/product'){
+         res.end('This is the PRODUCT');
+     } else {
+         res.writeHead(404, {
+             'Content-type': 'text/html',
+             'my-own-header': 'Hello World'
+         });
+         res.end('<h1>Page not found!</h1>');
+     }
+
 });
 
 server.listen(8000, '127.0.0.1', () => {
