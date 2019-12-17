6f7dd43bfd3743e3541396ee621ea6192c17f56d
Ye Zhan <lu@hooli.store>
2019-12-17 11:52:32 +0100

    building promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const superagent = require('superagent');
+
 // synchronous way
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 //   console.log(`Breed: ${data}`);
@@ -16,19 +17,43 @@ const superagent = require('superagent');
 // });
 
 // asynchronous way
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file!');
-      });
-    })
-    .catch(err => {
-      console.log(err.message);
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file :(');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) {
+        reject('Could not write file :(');
+        resolve('success');
+      }
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+    // fs.writeFile('dog-img.txt', res.body.message, err => {
+    //   if (err) return console.log(err.message);
+    //   console.log('Random dog image saved to file!');
+    // }); We change all this code for the then down
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch(err => {
+    console.log(err);
+  });
