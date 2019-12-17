79a39dd90d85ea708777e4a133ff23a6dde7bc4b
Ye Zhan <lu@hooli.store>
2019-12-17 12:02:12 +0100

    consuming promises with async await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -36,24 +36,40 @@ const writeFilePro = (file, data) => {
     });
   });
 };
-
-readFilePro(`${__dirname}/dog.txt`)
-  .then(data => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then(res => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro('dog-img.txt', res.body.message);
-    // fs.writeFile('dog-img.txt', res.body.message, err => {
-    //   if (err) return console.log(err.message);
-    //   console.log('Random dog image saved to file!');
-    // }); We change all this code for the then down
-  })
-  .then(() => {
+
+    await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file!');
-  })
-  .catch(err => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//     // fs.writeFile('dog-img.txt', res.body.message, err => {
+//     //   if (err) return console.log(err.message);
+//     //   console.log('Random dog image saved to file!');
+//     // }); We change all this code for the one then down
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file!');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
