0677d47d897ba2bc4c27236f12cea1d342a40f89
Ye Zhan <lu@hooli.store>
2019-12-17 12:44:18 +0100

    waiting for multiple promises simultaneously

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -20,7 +20,7 @@ const superagent = require('superagent');
 const readFilePro = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject('I could not find that file :(');
+      if (err) reject('I could not find that file 😢');
       resolve(data);
     });
   });
@@ -29,10 +29,8 @@ const readFilePro = file => {
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, err => {
-      if (err) {
-        reject('Could not write file :(');
-        resolve('success');
-      }
+      if (err) reject('Could not write file :(');
+      resolve('success');
     });
   });
 };
@@ -41,19 +39,37 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+    console.log(imgs);
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY 🐶';
 };
-getDogPic();
 
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then(data => {
 //     console.log(`Breed: ${data}`);
