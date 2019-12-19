47755ab03552c736a3bdb166407e70537d1222e3
Ye Zhan <lu@hooli.store>
2019-12-19 11:21:34 +0100

    using third party middlewares : morgan

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) middlewares:
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2)Route handlers
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -107,6 +114,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -117,6 +126,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) start server
 
 const port = 3000;
 app.listen(port, () => {
