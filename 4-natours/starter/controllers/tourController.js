a5fcfd822215fd70bcce81e91477853897b04591
Ye Zhan <lu@hooli.store>
2019-12-30 16:10:14 +0100

    sorting implemented for the api

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,23 +2,30 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD query
-    // 1) filtering
+    // 1a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // Advanced filtering
+    // 1b) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    // {difficulty: easy, duration: {$gte: 5}}
-    // { difficulty: 'easy', duration: { gte: '5' } }
+    let query = Tour.find(JSON.parse(queryStr));
+    // 2) Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     //Execute query
 
-    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     // Send Response
