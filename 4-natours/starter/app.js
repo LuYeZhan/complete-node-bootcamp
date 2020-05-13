4f80b42e6de0cfcc43a7718c3299697416369655
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-13 17:52:38 +0200

    handling unhandled routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👏 ');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,6 +22,14 @@ app.use((req, res, next) => {
 // 3) Routes (Here is where we mount our routes)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// ERROR HANDLING 404 function
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
 
 // 4) start server
 
