ea443122936515b3c53eddbfc4a2ec8869cf79de
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-14 17:37:57 +0200

    Errors outside express: Unhandled Rejection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -14,26 +14,21 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
   })
   .then(() => {
     console.log('DB connection successful!');
   });
 
-// const testTour = new Tour({
-//   name: 'The Park Camper',
-//   price: 997
-// });
-
-// testTour
-//   .save()
-//   .then(doc => {
-//     console.log(doc);
-//   })
-//   .catch(err => console.log('ERROR ❗ ', err));
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
