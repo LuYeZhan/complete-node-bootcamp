f626e6f400c1c91f2e46614620ef5509b0a7412e
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-20 14:47:56 +0200

    Modelling tour guides: embedding

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+const User = require('./userModel');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -103,7 +104,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -121,6 +123,12 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
+  next();
+});
 // middlewares running before and post creation
 
 // tourSchema.pre('save', function(next) {
