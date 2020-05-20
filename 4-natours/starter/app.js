5a7c6ff3ac2b47ca4d66526fdda244b478f39268
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-20 18:32:40 +0200

    Creating and getting reviews

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 // 1) Global middlewares:
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // 3) Routes (Here is where we mount our routes)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // ERROR HANDLING 404 function
 app.all('*', (req, res, next) => {
