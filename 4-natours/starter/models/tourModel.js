6ce175f9854630716a3e423cd0d626f5322e5b98
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-21 11:27:14 +0200

    virtual populate: Tours and Reviews

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -122,6 +122,13 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// this is virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // Document middleware: runs before .save() and .create(), not for update
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
