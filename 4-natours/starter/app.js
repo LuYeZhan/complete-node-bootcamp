0187c32ea9e0d7e6f44bb4309b7adb1bdaa27a0e
Lu Ye Zhan <lu.ye.external@axa.com>
2020-05-26 18:04:41 +0200

    setting up pug in express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,7 +14,14 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) Global middlewares:
+// access to static files on our public folder, serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set Security HTTP headers
 app.use(helmet());
 
@@ -53,9 +61,6 @@ app.use(
   })
 );
 
-// access to static files on our public folder, serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -64,6 +69,10 @@ app.use((req, res, next) => {
 });
 
 // 3) Routes (Here is where we mount our routes)
+app.get('/', (req, res, next) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
