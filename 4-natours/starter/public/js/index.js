f390a5b5ddef2080af04d427aafab74b0a2e25fa
Lu Ye Zhan <lu.ye.external@axa.com>
2020-06-02 10:53:22 +0200

    logging out users

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM elements
 
 const mapBox = document.getElementById('map');
-
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 // Values
 
@@ -27,3 +27,5 @@ if (loginForm)
     const password = document.getElementById('password').value;
     login(email, password);
   });
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
