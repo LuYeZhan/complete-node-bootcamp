06cae94aa63b427ba87211a86e56dfe66b909600
Lu Ye Zhan <lu.ye.external@axa.com>
2020-06-02 16:02:40 +0200

    Updating user passwords with API

--- 4-natours/starter/public/js/updateSettings.js
+++ 4-natours/starter/public/js/updateSettings.js
@@ -3,35 +3,26 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
 
     console.log(res);
   } catch (err) {
     showAlert('error', err.response.data.message);
-  }
-};
-
-export const logout = async () => {
-  try {
-    const res = await axios({
-      method: 'GET',
-      url: 'http://localhost:3000/api/v1/users/logout'
-    });
-    if ((res.data.status = 'success')) location.reload(true);
-  } catch (err) {
-    showAlert('error', 'Error logging out! Try again.');
   }
 };
