924afd7be77b0799faefafed2c5e49b452a26dae
Lu Ye Zhan <lu.ye.external@axa.com>
2020-06-04 14:04:56 +0200

    Processing payments on the front end

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -3,7 +3,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
-
+import { bookTour } from './stripe';
 // DOM elements
 
 const mapBox = document.getElementById('map');
@@ -11,6 +11,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // Values
 
@@ -62,4 +63,10 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
